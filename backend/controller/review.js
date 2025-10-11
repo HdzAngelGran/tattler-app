d206fdd7ce22ead509b3df60653be1aad1cc6e8b
arkn37.dev <arkn37.dev@outlook.com>
2025-10-10 19:02:19 -0600

    refactor: add validation for restaurantId and not allow a review from the restaurant owner

--- backend/controller/review.js
+++ backend/controller/review.js
@@ -1,3 +1,5 @@
+import TattlerError from '../middleware/tattlerError.js'
+import { Restaurant } from '../model/restaurant.model.js'
 import { Review } from '../model/review.model.js'
 
 class ReviewController {
@@ -14,10 +16,13 @@ class ReviewController {
   add = async (req, res) => {
     const { restaurantId } = req.params
     const { body } = req
-
     body.restaurant = restaurantId
+    const restaurant = await Restaurant.findById(restaurantId)
+    if (!restaurant) throw new TattlerError(404, 'Restaurant not found')
+    if (restaurant.createdBy.toString() === body.createdBy)
+      throw new TattlerError(400, 'You cannot review your own restaurant')
     await this.model.create(body)
-    res.status(200).send('Review posted successfully!')
+    res.status(200).json({ message: 'Review posted successfully!' })
   }
 }
 
