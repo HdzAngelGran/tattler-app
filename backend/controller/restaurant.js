a31f85442efe706c6d4d08c5c1f59b770c567c33
arkn37.dev <arkn37.dev@outlook.com>
2025-10-12 12:15:26 -0600

    refactor: add filter to get all and add pagination - filters: name, cuisine, priceRange and rate - Define size and page, default size 10 and page 1

--- backend/controller/restaurant.js
+++ backend/controller/restaurant.js
@@ -6,8 +6,64 @@ class RestaurantController {
   }
 
   getAll = async (req, res) => {
-    const restaurants = await this.model.find()
-    res.status(200).json(restaurants)
+    const page = parseInt(req.query.page) || 1
+    const size = parseInt(req.query.size) || 10
+    const startIndex = (page - 1) * size
+
+    const { name, cuisine, priceRange, rate } = req.query
+    const filter = {}
+
+    if (name) {
+      filter.name = { $regex: name, $options: 'i' }
+    }
+
+    if (cuisine) {
+      filter.cuisine = cuisine
+    }
+
+    if (priceRange) {
+      filter.priceRange = parseInt(priceRange)
+    }
+
+    const aggregation = [
+      {
+        $lookup: {
+          from: 'reviews',
+          localField: '_id',
+          foreignField: 'restaurant',
+          as: 'reviews',
+        },
+      },
+      {
+        $addFields: {
+          averageRating: { $avg: '$reviews.rating' },
+        },
+      },
+      {
+        $match: filter,
+      },
+    ]
+
+    if (rate) {
+      aggregation.push({
+        $match: { averageRating: { $gte: parseInt(rate) } },
+      })
+    }
+
+    const total = (await this.model.aggregate(aggregation)).length
+    const restaurants = await this.model
+      .aggregate(aggregation)
+      .skip(startIndex)
+      .limit(size)
+
+    const pagination = {
+      total,
+      size,
+      currentPage: page,
+      totalPages: Math.ceil(total / size),
+    }
+
+    res.status(200).json({ restaurants, pagination })
   }
 
   add = async (req, res) => {
