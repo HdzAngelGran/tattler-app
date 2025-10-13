d11160b7b5a3d0fa648ea1b70a9c9bd7dba750a5
arkn37.dev <arkn37.dev@outlook.com>
2025-10-13 15:07:45 -0600

    style: add theme for primeflex

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import 'primereact/resources/themes/lara-dark-purple/theme.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
+import 'primeflex/themes/primeone-dark.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
