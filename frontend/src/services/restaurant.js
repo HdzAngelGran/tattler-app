61aa97d1bde50489a79b2cb2eaf4ece5bd416b80
arkn37.dev <arkn37.dev@outlook.com>
2025-10-13 00:25:00 -0600

    feat: add restaurant services

---
+++ frontend/src/services/restaurant.js
@@ -0,0 +1,25 @@
+const baseUrl = '/api/v1/restaurant'
+
+export const getAllRestaurants = async (filters) => {
+  const URL = baseUrl + '?' + new URLSearchParams(filters)
+  const response = await fetch(URL)
+  if (!response.ok) {
+    throw new Error('Failed to fetch restaurants')
+  }
+  return response.json()
+}
+
+export const addRestaurant = async (restaurantData, token) => {
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(restaurantData),
+  })
+  if (!response.ok) {
+    throw new Error('Failed to add restaurant')
+  }
+  return response.json()
+}
