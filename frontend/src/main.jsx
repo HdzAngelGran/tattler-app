33e492ab415f0165e3ceb5c2fdb53d434fbc3d97
arkn37.dev <arkn37.dev@outlook.com>
2025-10-13 00:23:03 -0600

    feat: add proxy for auto complete path when fetching

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,16 @@ import 'primereact/resources/themes/lara-dark-purple/theme.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PrimeReactProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </PrimeReactProvider>
   </StrictMode>
 )
